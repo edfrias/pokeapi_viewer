8c76b7fd56a9dd9a969cfe0fe79ba7c54f14d874
Edgar Frías <edfriasji@gmail.com>
2024-10-20 19:05:01 +0200

    Add load-more button visibility control fix offset issue

--- js/main.js
+++ js/main.js
@@ -31,11 +31,23 @@ async function initApp() {
 
 const loadNextPokemonBatchAndUpdateUI = async () => {
   const pokemonListNode = document.getElementById('pokemon-list');
+  const loadMoreButton = document.getElementById('load-more');
+
+  if(state.currentOffset >= state.totalPokemon) {
+    loadMoreButton.style.display = 'none';
+    return;
+  }
 
   await getPokemonFetchedData({ pokemonList: state.allPokemon,
-    offset: state.currentOffset + state.limit, limit: state.limit });
+    offset: state.currentOffset, limit: state.limit });
 
-  state.currentOffset = state.pokemonList.length;
+  state.currentOffset += state.limit;
+
+  if(state.currentOffset >= state.totalPokemon) {
+    loadMoreButton.style.display = 'none';
+  } else {
+    loadMoreButton.style.display = 'block';
+  }
 
   renderPokemonsList({ pokemonList: state.pokemonList, node: pokemonListNode });
 };
